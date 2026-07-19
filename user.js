b51aa8d0c3fda1f1c3f015c3fe359875b02a4699
oanh19721172-collab <oanh19721172@gmail.com>
2026-07-19 21:39:15 +0700

    Create user.js

---
+++ user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         KataBump Auto Reg (Auto Bypass Captcha)
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Tự động hóa Katabump -  Bypass Altcha Captcha
+// @author       Lil Alex
+// @match        *://*.katabump.fr/*
+// @match        *://*.katabump.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_cookie
+// @grant        GM_registerMenuCommand
+// @grant        unsafeWindow
+// @connect      *
+// @run-at       document-idle
+// @icon         https://i.postimg.cc/yxc1Hc6v/image.png
+// @downloadURL  https://github.com/accdaune/Bypass/raw/main/volcano.user.js
+// @updateURL    https://github.com/accdaune/Bypass/raw/main/volcano.user.js
+// @require      https://github.com/accdaune/Bypass/raw/main/bypass.js
+// ==/UserScript==
